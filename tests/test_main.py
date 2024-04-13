7d7247afee226010f1352b0d401400b21d64c129
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-13 12:27:08 -0500

    Switched to `xfail` terminology

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,25 @@
-from hacker_functions.main import *
+import pytest
+
+from hacker_functions.main import (
+    authenticate,
+    average,
+    capitalize_first,
+    categorize_grade,
+    contains_an_a,
+    convert_to_usd,
+    count_matches,
+    find_in_list,
+    find_median,
+    find_middle,
+    is_valid_email,
+    max_number,
+    pair_items_in_list,
+    palindrome_checker,
+    run_command,
+    strings_equal,
+    sum_of_digits,
+    triangle_type,
+)
 
 
 def test_average_1() -> None:
@@ -9,13 +30,10 @@ def test_average_2() -> None:
     assert average([2, 2, 2]) == 2.0
 
 
+# ZeroDivisionError
+@pytest.mark.xfail
 def test_average_3() -> None:
-    try:
-        assert average([])
-    except ZeroDivisionError:
-        pass
-    else:
-        assert False
+    assert average([])
 
 
 def test_contains_a_1() -> None:
@@ -26,13 +44,10 @@ def test_contains_a_2() -> None:
     assert contains_an_a("halo") == True
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_contains_a_3() -> None:
-    try:
-        assert contains_an_a("b" * 1000 + "a") == True
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert contains_an_a("b" * 1000 + "a") == True
 
 
 def test_triangle_type_1() -> None:
@@ -45,12 +60,12 @@ def test_triangle_type_2() -> None:
 
 def test_triangle_type_3() -> None:
     assert triangle_type(3, 4, 5) == "Scalene"
-    try:
-        assert triangle_type(3, 4, 3) == "Isosceles"
-    except AssertionError:
-        pass
-    else:
-        assert False
+
+
+# AssertionError
+@pytest.mark.xfail
+def test_triangle_type_4() -> None:
+    assert triangle_type(3, 4, 3) == "Isosceles"
 
 
 def test_find_middle_1() -> None:
@@ -65,13 +80,10 @@ def test_find_middle_3() -> None:
     assert find_middle(10, 4, 8) == 8
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_find_middle_4() -> None:
-    try:
-        assert find_middle(10, 4, 10) == 10
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert find_middle(10, 4, 10) == 10
 
 
 def test_categorize_grade_1() -> None:
@@ -82,13 +94,10 @@ def test_categorize_grade_2() -> None:
     assert categorize_grade(95) == "A"
 
 
+# AssetionError
+@pytest.mark.xfail
 def test_categorize_grade_3() -> None:
-    try:
-        assert categorize_grade(97) == "A+"
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert categorize_grade(97) == "A+"
 
 
 def test_palindrome_checker_1() -> None:
@@ -99,13 +108,10 @@ def test_palindrome_checker_2() -> None:
     assert palindrome_checker("racecar") == True
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_palindrome_checker_3() -> None:
-    try:
-        assert palindrome_checker("yummy") == False
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert palindrome_checker("yummy") == False
 
 
 def test_count_matches_1() -> None:
@@ -116,13 +122,10 @@ def test_count_matches_2() -> None:
     assert count_matches(["hey", "you"], "hey") == 1
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_count_matches_3() -> None:
-    try:
-        assert count_matches(["hey", "hey", "hey"], "hey") == 3
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert count_matches(["hey", "hey", "hey"], "hey") == 3
 
 
 def test_capitalize_first_1() -> None:
@@ -133,13 +136,10 @@ def test_capitalize_first_2() -> None:
     assert capitalize_first("eric") == "Eric"
 
 
+# IndexError
+@pytest.mark.xfail
 def test_capitalize_first_3() -> None:
-    try:
-        assert capitalize_first("")
-    except IndexError:
-        pass
-    else:
-        assert False
+    assert capitalize_first("")
 
 
 def test_find_in_list_1() -> None:
@@ -150,13 +150,10 @@ def test_find_in_list_2() -> None:
     assert find_in_list([1, 3, 5, 7], 4) == False
 
 
+# Assertion Error
+@pytest.mark.xfail
 def test_find_in_list_3() -> None:
-    try:
-        assert find_in_list([7, 5, 3, 1], 3) == True
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert find_in_list([7, 5, 3, 1], 3) == True
 
 
 def test_strings_equal_1() -> None:
@@ -167,13 +164,10 @@ def test_strings_equal_2() -> None:
     assert strings_equal("shops", "ships") == False
 
 
+# Index Error
+@pytest.mark.xfail
 def test_strings_equal_3() -> None:
-    try:
-        assert strings_equal("hellothere", "hello") == False
-    except IndexError:
-        pass
-    else:
-        assert False
+    assert strings_equal("hellothere", "hello") == False
 
 
 def test_max_number_1() -> None:
@@ -184,13 +178,10 @@ def test_max_number_2() -> None:
     assert max_number([3, 5, 7, 1]) == 7
 
 
+# IndexError
+@pytest.mark.xfail
 def test_max_number_3() -> None:
-    try:
-        assert max_number([]) == True
-    except IndexError:
-        pass
-    else:
-        assert False
+    assert max_number([]) == True
 
 
 def test_run_command_1() -> None:
@@ -201,13 +192,10 @@ def test_run_command_2() -> None:
     assert run_command("remove", is_admin=False) == "Sorry, that's only for admin"
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_run_command_3() -> None:
-    try:
-        assert run_command("rEmovE", is_admin=False) == "Sorry, that's only for admin"
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert run_command("rEmovE", is_admin=False) == "Sorry, that's only for admin"
 
 
 def test_is_valid_email_1() -> None:
@@ -218,13 +206,10 @@ def test_is_valid_email_2() -> None:
     assert is_valid_email("tmp.com") == False
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_is_valid_email_3() -> None:
-    try:
-        assert is_valid_email("tmp.e@com") == True
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert is_valid_email("tmp.e@com") == True
 
 
 def test_sum_of_digits_1() -> None:
@@ -235,13 +220,10 @@ def test_sum_of_digits_2() -> None:
     assert sum_of_digits(123) == 6
 
 
+# ValueError
+@pytest.mark.xfail
 def test_sum_of_digits_3() -> None:
-    try:
-        assert sum_of_digits(-123) == 5
-    except ValueError:
-        pass
-    else:
-        assert False
+    assert sum_of_digits(-123) == 5
 
 
 def test_convert_to_usd_1() -> None:
@@ -252,13 +234,10 @@ def test_convert_to_usd_2() -> None:
     assert convert_to_usd(100, "JPY") == 0.91
 
 
+# KeyError
+@pytest.mark.xfail
 def test_convert_to_usd_3() -> None:
-    try:
-        assert convert_to_usd(10, "HSD") == 10
-    except KeyError:
-        pass
-    else:
-        assert False
+    assert convert_to_usd(10, "HSD") == 10
 
 
 def test_authenticate_1() -> None:
@@ -269,26 +248,20 @@ def test_authenticate_2() -> None:
     assert authenticate("secret") == True
 
 
+# IndexError
+@pytest.mark.xfail
 def test_authenticate_3() -> None:
-    try:
-        assert authenticate("long password")
-    except IndexError:
-        pass
-    else:
-        assert False
+    assert authenticate("long password")
 
 
 def test_pair_items_in_list_1() -> None:
     assert pair_items_in_list([1, 2, 5, 9, 0, 0]) == [(1, 2), (5, 9), (0, 0)]
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_pair_items_in_list_2() -> None:
-    try:
-        assert pair_items_in_list([1, 2, 3]) == [(1, 2), (3, None)]
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert pair_items_in_list([1, 2, 3]) == [(1, 2), (3, None)]
 
 
 def test_find_median_1() -> None:
@@ -299,10 +272,7 @@ def test_find_median_2() -> None:
     assert find_median([1, 2, 2, 5]) == 2
 
 
+# AssertionError
+@pytest.mark.xfail
 def test_find_median_3() -> None:
-    try:
-        assert find_median([1, 2, 4, 5]) == 3
-    except AssertionError:
-        pass
-    else:
-        assert False
+    assert find_median([1, 2, 4, 5]) == 3
